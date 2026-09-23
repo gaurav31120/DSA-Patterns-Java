e26afa4c79a4e1e0a677256f171b9af732d48b7a
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-23 23:41:52 +0530

    feat(dsa-03-fast-slow): add P016-sort-list - approach-02 merge sort -- pending -- will do in recursion

---
+++ 03-Fast-and-Slow-Pointers/16-sort-list/_02_MergeSort.java
@@ -0,0 +1,46 @@
+
+// Parked — learn during Recursion
+
+
+
+// public class _02_MergeSort {
+
+//     static Node sortList(Node head) {
+
+//     }
+
+//     public static void main(String[] args) {
+
+//         Node head = new Node(4);
+//         head.next = new Node(2);
+//         head.next.next = new Node(1);
+//         head.next.next.next = new Node(3);
+
+//         Node curr = sortList(head);
+
+//         System.out.print("Sorted list: ");
+
+//         while (curr != null) {
+
+//             System.out.print(curr.data);
+
+//             curr = curr.next;
+
+//             if (curr != null) {
+//                 System.out.print(" -> ");
+//             }
+//         }
+//     }
+    
+// }
+
+// class Node {
+
+//     int data;
+//     Node next;
+
+//     Node(int val) {
+//         this.data = val;
+//         this.next = null;
+//     }
+// }
